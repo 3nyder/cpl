bdfc1765a0825fc340159fcb650c183a45bc014c
José Manuel Snyder <code@3nyder.com>
2016-02-22 09:03:13 -0600

    connection to the database, done with help of https://www.terlici.com/2015/08/13/mysql-node-express.html

--- db.js
+++ db.js
@@ -22,29 +22,32 @@ exports.connect = function(mode, done) {
 };
 
 exports.get = function() {
-  return state.pool
-}
+  return state.pool;
+};
 
+//takes a JSON object and loads its data into the database
 exports.fixtures = function(data) {
-  var pool = state.pool
-  if (!pool) return done(new Error('Missing database connection.'))
+  var pool = state.pool;
+  if (!pool) return done(new Error('Missing database connection.'));
 
-  var names = Object.keys(data.tables)
+  var names = Object.keys(data.tables);
   async.each(names, function(name, cb) {
     async.each(data.tables[name], function(row, cb) {
-      var keys = Object.keys(row)
-        , values = keys.map(function(key) { return "'" + row[key] + "'" })
+      var keys = Object.keys(row),
+          values = keys.map(function(key) {
+            return "'" + row[key] + "'";
+          });
+      pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb);
+    }, cb);
+  }, done);
+};
 
-      pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
-    }, cb)
-  }, done)
-}
-
+//clears the data, but not the schemas from all the tables that you want.
 exports.drop = function(tables, done) {
-  var pool = state.pool
-  if (!pool) return done(new Error('Missing database connection.'))
+  var pool = state.pool;
+  if (!pool) return done(new Error('Missing database connection.'));
 
   async.each(tables, function(name, cb) {
-    pool.query('DELETE * FROM ' + name, cb)
-  }, done)
-}
+    pool.query('DELETE * FROM ' + name, cb);
+  }, done);
+};
