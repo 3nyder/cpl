161269a14bddf8ef0792721906638daa43f769ce
José Manuel Snyder <code@3nyder.com>
2016-04-28 23:32:52 -0500

    renamed comment for result and new service for get all lessons from a course

--- controllers/course.js
+++ controllers/course.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router  = express.Router();
 var course  = require('../models/course');
+var lesson  = require('../models/lesson');
 var types   = require('../helpers/types');
 var auth    = require('../middlewares/auth');
 
@@ -8,28 +9,28 @@ router.post('/', auth, function(req, res) {
     course_name = req.body.course_name;
     description = req.body.description;
 
-    course.create(course_name, description, function(err, comment) {
-        types.makeJSON(res, { idcourse: comment });
+    course.create(course_name, description, function(err, result) {
+        types.makeJSON(res, { idcourse: result });
     });
 });
 
 router.get('/', function(req, res) {
-    course.all(function (err, comment) {
-        types.makeJSON(res, comment);
+    course.all(function (err, result) {
+        types.makeJSON(res, result);
     });
 });
 
 router.get('/:id', function(req, res) {
     id = req.params.id;
-    course.get(id, function (err, comment) {
-        types.makeJSON(res, comment);
+    course.get(id, function (err, result) {
+        types.makeJSON(res, result);
     });
 });
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', auth, function(req, res) {
     id = req.params.id;
-    course.delete(id, function (err, comment) {
-        types.makeJSON(res, comment);
+    course.delete(id, function (err, result) {
+        types.makeJSON(res, result);
     });
 });
 
@@ -42,6 +43,13 @@ router.put('/:id', auth, function(req, res) {
 
     types.makeJSON(res, asked);
 
+});
+
+router.get('/:id/lessons', function(req, res) {
+    id = req.params.id;
+    lesson.getByCourse(id, function (err, result) {
+        types.makeJSON(res, result);
+    });
 });
 
 
