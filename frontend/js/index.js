8aba33f259122db2a716bdc6220c14fc805acc77
José Manuel Snyder <code@3nyder.com>
2016-05-11 00:44:34 -0500

    Changed string append for array push and then join the array, also click listener for the courses

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -10,31 +10,53 @@ $(function(){
         $divContent.empty();
         $nav.empty();
 
-        var cards = "";
-        var nLink = "";
+        var cards = [];
+        var nLink = [];
         for (var i = 0; i < data.length; i++) {
             var elem = data[i];
-            cards += '<div class="mdl-cell mdl-cell--4-col">';
-                cards += '<div class="course-card mdl-card mdl-shadow--2dp">';
-                    cards += '<div class="mdl-card__title mdl-card--expand">';
-                        cards += '<h4>'+elem.course+'</h4>';
-                    cards += '</div>';
-                    cards += '<div class="mdl-card__supporting-text">'+elem.description+'</div>';
-                    cards += '<div class="mdl-card__actions mdl-card--border">';
-                        cards += '<a data-idcourse="'+elem.idcourse+'" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">';
-                            cards += 'Iniciar Curso';
-                        cards += '</a>';
-                        cards += '<div class="mdl-layout-spacer"></div>';
-                        cards += '<i class="material-icons">library_books</i>';
-                    cards += '</div>';
-                cards += '</div>';
-            cards += '</div>';
+            cards.push('<div class="mdl-cell mdl-cell--4-col">');
+                cards.push('<div class="course-card mdl-card mdl-shadow--2dp">');
+                    cards.push('<div class="mdl-card__title mdl-card--expand">');
+                        cards.push('<h4>'+elem.course+'</h4>');
+                    cards.push('</div>');
+                    cards.push('<div class="mdl-card__supporting-text">'+elem.description+'</div>');
+                    cards.push('<div class="mdl-card__actions mdl-card--border">');
+                        cards.push('<a data-idcourse="'+elem.idcourse+'" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">');
+                            cards.push('Iniciar Curso');
+                        cards.push('</a>');
+                        cards.push('<div class="mdl-layout-spacer"></div>');
+                        cards.push('<i class="material-icons">library_books</i>');
+                    cards.push('</div>');
+                cards.push('</div>');
+            cards.push('</div>');
 
-            nLink += '<a class="mdl-navigation__link" data-idcourse="'+elem.idcourse+'">'+elem.course+'</a>';
+            nLink.push('<a class="mdl-navigation__link" data-idcourse="'+elem.idcourse+'">'+elem.course+'</a>');
         }
 
-        $divContent.append(cards);
-        $nav.append(nLink);
+        $divContent.append(cards.join(""));
+        $nav.append(nLink.join(""));
+    });
+
+    $('body').on('click', '.course-card a', function() {
+        var idcourse = $(this).data('idcourse');
+        $.getJSON(base+'/courses/'+idcourse+'/lessons', function(data) {
+            var lessons = [];
+
+            for (var i = 0; i < data.length; i++) {
+                var elem = data[i];
+                lessons.push('<li class="mdl-list__item">');
+                    lessons.push('<span class="mdl-list__item-primary-content">');
+                        lessons.push(elem.lesson);
+                    lessons.push('</span>');
+                lessons.push('</li>');
+            }
+
+            $( "<ul/>", {
+                "class": "mdl-list",
+                html: lessons.join("")
+            }).appendTo(".cpl-content");
+
+        });
     });
 
 });
